fa368ab7065b13bd02bbc127226b25e5dd6f00cd
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-02-08 11:52:00 +0100

    feat: linting and promise based injectable getter

--- src/components/XlsxParse.vue
+++ src/components/XlsxParse.vue
@@ -12,56 +12,71 @@ export default {
       default: null
     }
   },
-  data () {
+  data() {
     return {
       libLoaded: false
     };
   },
   computed: {
-    loadedAndfile () {
+    loadedAndfile() {
       return this.libLoaded ? this.file : null;
     }
   },
   watch: {
     loadedAndfile: {
       immediate: true,
-      handler (file) {
+      handler(file) {
         if (file) {
           this.parseFile(file);
         }
       }
     }
   },
-  mounted () {
+  mounted() {
     this.load();
   },
+  provide() {
+    return {
+      getWorkbook: this.getWorkbook
+    };
+  },
   methods: {
-    async load () {
-      this._xlsx = await import('xlsx');
+    async load() {
+      this._xlsx = await import("xlsx");
       this.libLoaded = true;
     },
-    parseFile (file) {
+    parseFile(file) {
       const reader = new FileReader();
       reader.onload = e => {
-        let binary = '';
+        let binary = "";
         const bytes = new Uint8Array(e.target.result);
         const length = bytes.byteLength;
         for (var i = 0; i < length; i++) {
           binary += String.fromCharCode(bytes[i]);
         }
-        this._workbook = this._xlsx.read(binary, { type: 'binary' });
+        this._workbook = this._xlsx.read(binary, { type: "binary" });
         console.log(this._workbook);
-        this.$emit('parsed', this._workbook);
+        this.$emit("parsed", this._workbook);
+        if (this._resolve) {
+          this._resolve(this._workbook);
+        }
       };
       reader.onerror = e => {
         console.log(e);
+        if (this._reject) {
+          this._reject(this._workbook);
+        }
       };
       reader.readAsArrayBuffer(file);
+    },
+    getWorkbook() {
+      return new Promise((resolve, reject) => {
+        this._resolve = resolve;
+        this._reject = reject;
+      });
     }
   }
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
