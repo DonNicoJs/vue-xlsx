2ceeba5258663efca716e601ce2d263cd6a3f261
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-02-13 16:25:09 +0100

    feat: fix example component to test multiple sheets

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
   <div class="VueXlsx">
-    <xlsx-write :collection="[{ a: 1, b: 2 }]">
-      <xlsx-download>
-        <button>Download</button>
-      </xlsx-download>
-    </xlsx-write>
-    <input type="file" @change="onChange" />
-    <xlsx-read :file="file">
-      <xlsx-sheets>
-        <template #default="{sheets}">
-          <select v-model="selectedSheet">
-            <option v-for="sheet in sheets" :key="sheet" :value="sheet">
-              {{ sheet }}
-            </option>
-          </select>
-        </template>
-      </xlsx-sheets>
-      <xlsx-table :sheet="selectedSheet" />
-      <xlsx-json :sheet="selectedSheet">
-        <template #default="{collection}">
-          <div>
-            {{ collection }}
-          </div>
-        </template>
-      </xlsx-json>
-    </xlsx-read>
+    <section>
+      <h3>Create XLSX</h3>
+      <div>
+        <input v-model="sheetName" placeholder="type a new sheet name" />
+        <button v-if="sheetName" @click="addSheet">Add Sheet</button>
+      </div>
+      <div>Sheets: {{ sheets }}</div>
+
+      <xlsx-workbook>
+        <xlsx-sheet
+          :collection="sheet.data"
+          v-for="sheet in sheets"
+          :key="sheet.name"
+          :sheet-name="sheet.name"
+        />
+        <xlsx-download>
+          <button>Download</button>
+        </xlsx-download>
+      </xlsx-workbook>
+    </section>
+    <hr />
+    <section>
+      <h3>Import XLSX</h3>
+      <input type="file" @change="onChange" />
+      <xlsx-read :file="file">
+        <xlsx-sheets>
+          <template #default="{sheets}">
+            <select v-model="selectedSheet">
+              <option v-for="sheet in sheets" :key="sheet" :value="sheet">
+                {{ sheet }}
+              </option>
+            </select>
+          </template>
+        </xlsx-sheets>
+        <xlsx-table :sheet="selectedSheet" />
+        <xlsx-json :sheet="selectedSheet">
+          <template #default="{collection}">
+            <div>
+              {{ collection }}
+            </div>
+          </template>
+        </xlsx-json>
+      </xlsx-read>
+    </section>
   </div>
 </template>
 
@@ -33,7 +52,8 @@ import XlsxRead from "./components/XlsxRead";
 import XlsxTable from "./components/XlsxTable";
 import XlsxSheets from "./components/XlsxSheets";
 import XlsxJson from "./components/XlsxJson";
-import XlsxWrite from "./components/XlsxWrite";
+import XlsxWorkbook from "./components/XlsxWorkbook";
+import XlsxSheet from "./components/XlsxSheet";
 import XlsxDownload from "./components/XlsxDownload";
 
 export default {
@@ -42,18 +62,26 @@ export default {
     XlsxTable,
     XlsxSheets,
     XlsxJson,
-    XlsxWrite,
+    XlsxWorkbook,
+    XlsxSheet,
     XlsxDownload
   },
   data() {
     return {
       file: null,
-      selectedSheet: null
+      selectedSheet: null,
+      sheetName: null,
+      sheets: [{ name: "SheetOne", data: [{ c: 2 }] }],
+      collection: [{ a: 1, b: 2 }]
     };
   },
   methods: {
     onChange(event) {
       this.file = event.target.files ? event.target.files[0] : null;
+    },
+    addSheet() {
+      this.sheets.push({ name: this.sheetName, data: [...this.collection] });
+      this.sheetName = null;
     }
   }
 };
