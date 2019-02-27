412d78f14e964de1039e1165071ed504952c2a48
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-02-27 15:06:54 +0100

    chore: index file to export all the modules

---
+++ src/index.js
@@ -0,0 +1,12 @@
+export * from "./utils";
+
+export { default as XlsxDownload } from "./components/XlsxDownload";
+export { default as XlsxJson } from "./components/XlsxJson";
+export { default as XlsxRead } from "./components/XlsxRead";
+export { default as XlsxSheet } from "./components/XlsxSheet";
+export { default as XlsxSheets } from "./components/XlsxSheets";
+export { default as XlsxTable } from "./components/XlsxTable";
+export { default as XlsxWorkbook } from "./components/XlsxWorkbook";
+
+export { default as SheetToMixin } from "./mixins/SheetTo";
+export { default as WorkbookHandlerMixin } from "./mixins/WorkbookHandler";
