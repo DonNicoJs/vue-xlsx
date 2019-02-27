634f362cd2ed3e8a98f97a9c736d665d5f9a435c
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-02-27 15:06:36 +0100

    feat: add babel plugin and rewrite rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,61 @@ import vue from "rollup-plugin-vue";
 import replace from "rollup-plugin-replace";
 import node from "rollup-plugin-node-resolve";
 import cjs from "rollup-plugin-commonjs";
-import buble from "rollup-plugin-buble";
+import babel from "rollup-plugin-babel";
+
+import copy from "rollup-copy-plugin";
+
+import fs from "fs";
+
+const baseFolder = "./src/";
+const componentFolder = "components/";
+const mixinFolder = "mixins/";
+
+const components = fs.readdirSync(baseFolder + componentFolder);
+const mixins = fs.readdirSync(baseFolder + mixinFolder);
+
+const mapEntry = (f, ext, folder) => ({
+  input: baseFolder + folder + f,
+  external: ["vue", "xlsx"],
+  output: {
+    format: "esm",
+    file: `dist/${folder}${f.replace(ext, "js")}`
+  },
+  plugins: [
+    replace({ "process.env.NODE_ENV": "production" }),
+    node({
+      extensions: [".vue", ".js"]
+    }),
+    cjs(),
+    vue({
+      css: true,
+      compileTemplate: true
+    }),
+    babel({
+      exclude: "node_modules/**",
+      runtimeHelpers: true
+    })
+  ]
+});
 
 export default [
+  ...components.map(f => mapEntry(f, "vue", componentFolder)),
+  ...mixins.map(f => mapEntry(f, "js", mixinFolder)),
   {
-    input: "./src/index.js",
+    input: "./src/utils.js",
     external: ["vue", "xlsx"],
     output: {
       format: "esm",
-      file: `dist/vue-xlsx.es.js`
+      file: `dist/utils.js`
     },
     plugins: [
       cjs(),
-      vue({
-        css: true,
-        compileTemplate: true
+      babel({
+        exclude: "node_modules/**",
+        runtimeHelpers: true
       }),
-      buble({
-        objectAssign: true
+      copy({
+        "src/index.js": "dist/vue-xlsx.es.js"
       })
     ]
   },
@@ -42,8 +79,9 @@ export default [
         css: true,
         compileTemplate: true
       }),
-      buble({
-        objectAssign: true
+      babel({
+        exclude: "node_modules/**",
+        runtimeHelpers: true
       })
     ]
   },
@@ -68,8 +106,9 @@ export default [
         css: true,
         compileTemplate: true
       }),
-      buble({
-        objectAssign: true
+      babel({
+        exclude: "node_modules/**",
+        runtimeHelpers: true
       })
     ]
   }
