a7c56a40ff390ec3f6a845cd7921ccff20b86c4e
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-03-10 11:22:40 +0100

    feat: move to babel/preset-env to rollup compatibility

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ["@vue/app"]
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage"
+      }
+    ]
+  ],
+  plugins: ["@babel/plugin-syntax-dynamic-import"]
 };
